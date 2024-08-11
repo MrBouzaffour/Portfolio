8d999c89fc3d00a833ce71c3bf12489fd467d066
Ahmed Bouzaffour <143547279+MrBouzaffour@users.noreply.github.com>
2024-08-11 14:11:36 -0600

    Update SkillsSection.vue

--- src/components/SkillsSection.vue
+++ src/components/SkillsSection.vue
@@ -2,8 +2,8 @@
   <section class="skills">
     <h2>Skills</h2>
     <ul>
-      <li><strong>Programming Languages:</strong> C ,C++ ,C# ,PHP ,Java ,Pascal, Python ,JavaScript Bash ,Pascal ,Risc-V</li>
-      <li><strong>Development Technologies :</strong> HTML ,CSS ,Nodejs, VueJs, Bootstrap, Docker, NGINX  </li>
+      <li><strong>Programming Languages: </strong> C ,C++ ,C# ,PHP ,Java ,Pascal ,Python ,JavaScript ,Bash ,Pascal ,Risc-V </li>
+      <li><strong>Development Technologies :</strong> HTML ,CSS ,Nodejs, VueJs, Bootstrap,Docker , NGINX  </li>
       <li><strong>Databases:</strong> MongoDB, SQL</li>
       <li><strong>Version Control:</strong> Git</li>
       <li><strong>Operating Systems:</strong> Windows, Linux</li>
